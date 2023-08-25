99be6549ecb9d6182ba6340a099daf06900a85fd
mijinB <bmj13465@gmail.com>
2023-08-25 18:05:39 +0900

    캐릭터 선택 기능 추가

--- src/js/character.js
+++ src/js/character.js
@@ -1,7 +1,30 @@
 const characterButtons = document.querySelector(".character-buttons");
+const loginText = document.querySelector("#login-form h2");
+const greetingText = document.querySelector("#greeting");
 
-const choiceIcon = (event) => {
-  console.log(event.target.innerText);
+const CHARACTER_KEY = "character";
+
+const userName = localStorage.getItem("username");
+let choiceCharacter = "🐰";
+
+const choiceButton = (event) => {
+  choiceCharacter = event.target.innerText;
+  
+  localStorage.setItem(CHARACTER_KEY, choiceCharacter);
+
+  greetingText.innerText = `${choiceCharacter} ${userName}\`s To Do`;
+  loginText.innerText = `${choiceCharacter} User Name`;
 }
 
-characterButtons.addEventListener("click", choiceIcon);
+const onLoadCharacter = () => {
+  const chosenCharacter = localStorage.getItem(CHARACTER_KEY);
+  if (chosenCharacter == null) {
+    choiceCharacter = "🐰";
+  } else {
+    greetingText.innerText = `${chosenCharacter} ${userName}\`s To Do`;
+    loginText.innerText = `${chosenCharacter} User Name`;
+  }
+}
+
+characterButtons.addEventListener("click", choiceButton);
+onLoadCharacter();
