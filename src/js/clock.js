c67b07ae62f0a0e1af4922c6db4a8a5327354e7b
mijinB <bmj13465@gmail.com>
2023-08-29 17:05:00 +0900

    clock.js 파일 함수 주석 추가

--- src/js/clock.js
+++ src/js/clock.js
@@ -1,6 +1,10 @@
 const clock = document.querySelector("span#clock");
 
-function getClock() {
+/**@function getClock
+ * 1. 현재 시, 분, 초 추출하여 한 글자면 앞에 "0"을 붙여 두 글자로 만들어서 변수에 대입
+ * 2. 시:분:초 형식으로 화면에 표출
+ */
+const getClock = () => {
   const date = new Date();
   const hours = String(date.getHours()).padStart(2, "0");
   const minutes = String(date.getMinutes()).padStart(2, "0");
@@ -10,4 +14,6 @@ function getClock() {
 }
 
 getClock();
+
+//1초마다 반복적으로 함수 실행
 setInterval(getClock, 1000);
