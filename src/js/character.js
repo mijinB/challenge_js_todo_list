3c069e983f41d1bd3bd33d3e2458e116a29d5071
mijinB <bmj13465@gmail.com>
2023-08-29 17:25:25 +0900

    character.js파일 주석 수정 / login.js파일 리팩토링+함수 주석 추가

--- src/js/character.js
+++ src/js/character.js
@@ -16,8 +16,8 @@ const paintText = (character, userName) => {
 
 /**@function onCharacterChoice
  * 1. lacalStorage에 저장되어 있는 사용자 이름을 가져오고
- * 2. 사용자가 클릭한 character 버튼을 감지해서 변수에 해당 character를 대입해주고
- * 3. 변수에 대입한 character를 lacalStorage에 저장
+ * 2. 사용자가 클릭한 character 버튼을 감지해서 전역 변수에 해당 character를 대입해주고
+ * 3. 전역 변수에 대입한 character를 lacalStorage에 저장
  * 4. paintText 함수 실행(화면에 표출)
  */
 const onCharacterChoice = (event) => {
@@ -31,7 +31,7 @@ const onCharacterChoice = (event) => {
 
 /**@function onLoadCharacter
  * 1. lacalStorage에 저장되어 있는 character와 userName을 가져오고
- * 2. lacalStorage에서 가져온 character가 있는지 확인 후 해당 character를 변수에 대입
+ * 2. lacalStorage에서 가져온 character가 있는지 확인 후 해당 character를 전역 변수에 대입
  * 3. paintText 함수 실행(화면에 표출)
  */
 const onLoadCharacter = () => {
--- src/js/login.js
+++ src/js/login.js
@@ -11,7 +11,11 @@ const USERNAME_KEY = "username";
 
 let character = "🐰";
 
-function paintGreetings(username) {
+/**@function paintGreetings
+ * 1. greeting Text와 todo List를 숨기기 해제하고 화면에 표출
+ * 2. games와 setting 폴더 disabled 해둔 거 해제하고 활성화
+ */
+const paintGreetings = (username) => {
   greeting.innerText = `${character} ${username}\`s To Do`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
   todo.classList.remove(HIDDEN_CLASSNAME);
@@ -20,20 +24,32 @@ function paintGreetings(username) {
   settingButton.disabled = false;
 }
 
-function onLoginSubmit(event) {
+/**@function onLoginSubmit
+ * 1. form submit감지 시 실행되는 기본 동작(페이지 새로고침) 막기
+ * 2. 사용자가 input에 입력한 username을 변수에 대입하고 localStorage에 저장
+ * 3. 로그인form 숨기기
+ * 4. paintGreetings 함수 실행(todo List 화면 표출)
+ */
+const onLoginSubmit = (event) => {
   event.preventDefault();
-
-  loginForm.classList.add(HIDDEN_CLASSNAME);
   
   const username = loginInput.value;
   localStorage.setItem(USERNAME_KEY, username);
 
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+
   paintGreetings(username);
 }
 
-const onUserName = () => {
+/**@function initializeUserName
+ * 1. localStorage에 저장되어 있는 username을 가져와서 변수에 대입
+ * 2. localStorage에 저장되어 있지 않으면 로그인form 숨기기 해제하고 화면 표출, "submit"이벤트리스너 추가
+ * 3. 저장되어 있으면 paintGreetings 함수 실행(todo List 화면 표출)
+ */
+const initializeUserName = () => {
   const savedUserName = localStorage.getItem(USERNAME_KEY);
-  if(savedUserName == null) {
+
+  if(!savedUserName) {
     loginForm.classList.remove(HIDDEN_CLASSNAME);
     loginForm.addEventListener("submit", onLoginSubmit);
   } else {
@@ -41,14 +57,17 @@ const onUserName = () => {
   }
 }
 
-const onCharacter = () => {
+/**@function initializeCharacter
+ * 1. localStorage에 저장되어 있는 character를 가져오고
+ * 2. lacalStorage에서 가져온 character가 있는지 확인 후 해당 character를 전역 변수에 대입
+ */
+const initializeCharacter = () => {
   const chosenCharacter = localStorage.getItem("character");
-  if (chosenCharacter == null) {
-    character = "🐰";
-  } else {
+  
+  if (chosenCharacter !== null) {
     character = chosenCharacter;
   }
 }
 
-onUserName();
-onCharacter();
+initializeUserName();
+initializeCharacter();
